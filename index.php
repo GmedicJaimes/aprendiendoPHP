133614941156febff9f9be86494db3705f73a3fd
Gulliano Jaimes <88950788+GmedicJaimes@users.noreply.github.com>
2023-10-24 10:34:01 -0500

    aprendiendo variables en php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php 
-  echo 'Hola mundo, daniel es una perra'; //toda instrucion que escribamos debe terminar siempre en ;
+  $nombre = 'Gulliano Medic Jaimes'
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Probando PHP</title>
+</head>
+<body>
+  <h2><?php echo "Hola mundo mi nombre es $nombre"; ?></h2>
+</body>
+</html>
