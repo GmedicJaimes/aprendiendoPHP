f6a925e853e6902dacdb631ade4973ff7624ff90
Gulliano Jaimes <88950788+GmedicJaimes@users.noreply.github.com>
2023-10-25 17:55:42 -0500

    estructuras condicionales en php

--- index.php
+++ index.php
@@ -1,26 +1,16 @@
 <?php 
-  //tipos de datos en php
-  $un_bool = TRUE;   // un valor booleano
-  $un_str  = "foo";  // una cadena de caracteres
-  $un_str2 = 'foo';  // una cadena de caracteres
-  $un_int  = 12;     // un número entero
+  $num1 = 5;
+  $num2 = 56;
 
-  echo gettype($un_bool); // imprime: boolean
-  echo '<br>';
-  echo gettype($un_str);  // imprime: string
-  echo '<br>';
-  echo gettype($un_int);
+  echo 'Primer parrafo <br>';
 
-  // Si este valor es un entero, incrementarlo en cuatro
-  if (is_int($un_int)) {
-      $un_int += 4;
-
-      echo "El nuevo numero es: " . $un_int;
+  if($num1 < $num2) {
+    echo 'Segundo parrafo <br>';
+  } elseif ($num1 > $num2){
+    echo 'Segundo - Primero parrafo <br>';
+  } else {
+    echo 'No se cumple nada';
   }
 
-  // Si $un_bool es una cadena, imprimirla
-  // (no imprime nada)
-  if (is_string($un_bool)) {
-      echo "String: $un_bool";
-  }
-?>
+  echo 'Tercer parrafo <br>';
+?> 
