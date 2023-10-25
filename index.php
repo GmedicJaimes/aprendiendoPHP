832c1aea252c7d052041987a338c617b4be81f46
Gulliano Jaimes <88950788+GmedicJaimes@users.noreply.github.com>
2023-10-25 16:25:20 -0500

    tipos de datos en php

--- index.php
+++ index.php
@@ -1,16 +1,26 @@
 <?php 
-  // $nombre = 'Gulliano Medic Jaimes'
+  //tipos de datos en php
+  $un_bool = TRUE;   // un valor booleano
+  $un_str  = "foo";  // una cadena de caracteres
+  $un_str2 = 'foo';  // una cadena de caracteres
+  $un_int  = 12;     // un número entero
 
-  //las constantes se declaran de la siguiente manera:
-  define('CURSO', 'Aprendiendo php 8 desde cero');
-  // echo CURSO;
+  echo gettype($un_bool); // imprime: boolean
+  echo '<br>';
+  echo gettype($un_str);  // imprime: string
+  echo '<br>';
+  echo gettype($un_int);
 
-  define('ANIMALES' , [
-    'perro',
-    'gato',
-    'loro',
-    'rinoceronte'
-  ]);
+  // Si este valor es un entero, incrementarlo en cuatro
+  if (is_int($un_int)) {
+      $un_int += 4;
 
-  echo ANIMALES[3];
+      echo "El nuevo numero es: " . $un_int;
+  }
+
+  // Si $un_bool es una cadena, imprimirla
+  // (no imprime nada)
+  if (is_string($un_bool)) {
+      echo "String: $un_bool";
+  }
 ?>
