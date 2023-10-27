14e3004b59cfc09193af7fbbb83e918152ab9000
Gulliano Jaimes <88950788+GmedicJaimes@users.noreply.github.com>
2023-10-27 16:13:37 -0500

    sentencias switch y match en php

--- index.php
+++ index.php
@@ -1,16 +1,50 @@
-<?php 
-  $num1 = 5;
-  $num2 = 56;
+<?php
+$num1 = 6;
+$num2 = 9;
 
-  echo 'Primer parrafo <br>';
+echo 'Primer parrafo <br>';
 
-  if($num1 < $num2) {
-    echo 'Segundo parrafo <br>';
-  } elseif ($num1 > $num2){
-    echo 'Segundo - Primero parrafo <br>';
-  } else {
-    echo 'No se cumple nada';
-  }
+if ($num1 < $num2) {
+  echo 'Segundo parrafo <br>';
+} elseif ($num1 > $num2) {
+  echo 'Segundo - Primero parrafo <br>';
+} else {
+  echo 'No se cumple nada <br>';
+}
 
-  echo 'Tercer parrafo <br>';
-?> 
+echo 'Tercer parrafo <br>';
+
+
+//operadores ternarios
+$valor = $num1 > $num2 ? 'mayor' : ($num1 < $num2 ? 'menor' : 'igual');
+echo $valor;
+echo '<br>';
+
+
+$color = 'verde';
+
+//seleccion multiple
+switch ($color) {
+  case 'azul':
+    echo 'Color del cielo';
+    break;
+
+  case 'amarillo':
+    echo 'Color del sol';
+    break;
+
+  case 'rojo':
+    echo 'Color que molesta a los toros';
+    break;
+
+  default:
+    echo 'Estas equivocado, selecciona un color valido';
+    break;
+}
+
+echo '<br>';
+
+echo match ($color) {
+  'azul' => 'Color del cielo',
+  'verde' => 'Color de las praderas',
+};
